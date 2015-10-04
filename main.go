6c818dcea9686ed83fd3b27aff9dc47c60b8618b
daveloper87 <dai.williams@bbc.co.uk>
2015-10-04 18:13:32 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/status", func(c *gin.Context) {
+		c.String(200, "OK")
+	})
+	router.Run(":9000")
+}
